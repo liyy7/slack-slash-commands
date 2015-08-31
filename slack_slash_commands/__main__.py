8713aa36a4c3f20cececea1ffdbb775ea56b936d
Yongyu Lee <aleeyyu+01@gmail.com>
2015-09-01 04:32:24 +0900

    changed reply message

--- slack_slash_commands/__main__.py
+++ slack_slash_commands/__main__.py
@@ -4,11 +4,28 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class CommandHandler(BaseHTTPRequestHandler):
+    def _get_parameters(self):
+        if not hasattr(self, '__parameters') or not self.__parameters:
+            query = urllib.parse.urlparse(self.path).query
+            self.__parameters = dict(qc.split('=') for qc in query.split('&')) if query else {}
+        return self.__parameters
+
+    def _get_parameter(self, parameter):
+        return self._get_parameters().get(parameter)
+
+    def _handle_command(self, command, text):
+        return '{} `{}`'.format(command, text)
+
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'You accessed path: ' + urllib.parse.unquote(self.path).encode('utf8'))
+        command = self._get_parameter('command')
+        text = self._get_parameter('text')
+        reply = self._handle_command(command, text)
+        if isinstance(reply, str):
+            reply = reply.encode()
+        self.wfile.write(b'Reply: ' + reply)
 
 
 def main():
